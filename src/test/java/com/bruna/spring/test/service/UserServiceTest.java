fa11f8f7d4c49a9e15d418b88f7381cfcf090483
brunatofani <bruna_tofani@yahoo.com.br>
2019-10-23 08:39:22 +0200

    Add files via upload

--- src/test/java/com/bruna/spring/test/service/UserServiceTest.java
+++ src/test/java/com/bruna/spring/test/service/UserServiceTest.java
@@ -8,9 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -22,16 +25,43 @@ public class UserServiceTest {
     UserRepository userRepository = mock(UserRepository.class) ;
 
     @Test
-    public void testfindUserByIdShouldFind() {
-        Long id = 1L;
+    public void testFindUserByIdShouldFind() {
         User user = getUser();
-        when(userRepository.findById(id)).thenReturn(java.util.Optional.ofNullable(user));
-        assertEquals("Teste User findUserById", user.getName(), userService.findUserById(id).getName());
+        when(userRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(user));
+        assertEquals("Teste User findUserById - ShouldFind", user.getName(), userService.findUserById(user.getIdUser()).getName());
+    }
+
+    @Test
+    public void testFindUserByIdShouldNotFind() {
+        User user = getUser();
+        when(userRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(user));
+        assertNotEquals("Teste User findUserById - ShouldNotFind", getUser1().getName(), userService.findUserById(user.getIdUser()).getName());
+    }
+
+    @Test
+    public void testGetUserByNameShouldFindOne() {
+        User user = getUser();
+        when(userRepository.findByName(anyString())).thenReturn(user);
+        assertEquals("Teste User findUserById - ShouldFind", user.getIdUser(), userService.getUserByName(user.getName()).getIdUser());
+    }
+
+    @Test
+    public void testGetUserByNameShouldNotFind() {
+        User user = getUser();
+        when(userRepository.findByName(anyString())).thenReturn(user);
+        assertNotEquals("Teste User findUserById - ShouldNotFind", getUser1().getIdUser(), userService.getUserByName(user.getName()).getIdUser());
     }
 
     private User getUser() {
         User user = new User();
         user.setName("test");
+        user.setIdUser(2L);
+        return user;
+    }
+
+    private User getUser1() {
+        User user = new User();
+        user.setName("test1");
         user.setIdUser(1L);
         return user;
     }
